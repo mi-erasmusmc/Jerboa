9f6012e6d3ae36a52e9c28f6e90e8fa2cbafa82a
bmosseveld <b.mosseveld@erasmusmc.nl>
2018-06-06 08:46:22 +0200

    Version 1.0.0.1: Fixed bug DrugUtilizationPrescriptionLevel of 1.0.0.0

--- src/org/erasmusmc/jerboa/config/Parameters.java
+++ src/org/erasmusmc/jerboa/config/Parameters.java
@@ -47,7 +47,7 @@ public class Parameters {
 	/*****************
 	  VERSION NUMBER
 	 ****************/
-	public static final String VERSION_NUMBER = "1.0.0.0";
+	public static final String VERSION_NUMBER = "1.0.0.1";
 	
 	/****************
 	  DATABASE NAMES
